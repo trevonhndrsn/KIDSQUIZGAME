a4fbdcdb92eaafb19b007cb521960ad65fa80d96
trevonhndrsn <103610962+trevonhndrsn@users.noreply.github.com>
2022-07-20 20:27:53 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,14 @@
+question_data = [
+    {"text": "Elsa is older than Anna.", "answer": "True"},
+    {"text": "Camels have Three sets of eyelids.", "answer": "True"},
+    {"text": "You can sneeze in your sleep. ", "answer": "False"},
+    {"text": "Noses are responsible for smelling.", "answer": "True"},
+    {"text": "Washington DC is in LONDON ENGLAND", "answer": "False"},
+    {"text": "You cannot cry in space", "answer": "True"},
+    {"text": "Pop its are made of cotton candy", "answer": "False"},
+    {"text": "Some dogs can speak english.", "answer": "False"},
+    {"text": "Babies are made from flowers.", "answer": "False"},
+    {"text": "Butterflies are not birds.", "answer": "True"},
+    {"text": "Your car can drive with water in the gas tank", "answer": "False"},
+    {"text": "You can drink Mac N Cheese.", "answer": "False"}
+]
